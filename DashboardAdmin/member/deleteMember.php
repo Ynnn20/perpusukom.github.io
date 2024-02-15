3862bad02dee68e9e7051f7aaa4c5116b9e7df74
Ynnn20 <hsymsyahputra99@gmail.com>
2024-02-15 14:36:37 +0700

    tdrtd

--- DashboardAdmin/member/deleteMember.php
+++ DashboardAdmin/member/deleteMember.php
@@ -4,12 +4,12 @@ require "../../config/config.php";
 $nisnMember = $_GET["id"];
 if(deleteMember($nisnMember) > 0) {
     echo "<script>
-    alert('Member berhasil dihapus!');
+    alert('Member gagal dihapus!');
     document.location.href = 'member.php';
     </script>";
   }else {
     echo "<script>
-    alert('Member gagal dihapus!');
+    alert('Member berhasil dihapus!');
     document.location.href = 'member.php';
     </script>";
 }
